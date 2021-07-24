53708dd203d4ec566c97a1f0a135f00a71638c44
Ana.Sousa <anasousa314@outlook.com>
2021-07-24 16:09:09 -0300

    aula27 sobre tratamento de erros com Error Hanlder e express-async-errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const routes = require('./routes')
+require('express-async-errors');//para tratar erros assincronos. yarn add express-async-errors
+
+const routes = require('./routes');
+
 const app = express();
 
 app.use(express.json());//assim consigo pegar o body da requisicao, precisa estar acima do use(routes)
 app.use(routes);
+
+// Usado para tratar erros sincronos. Deve ter obrigatoriamente e nessa ordem error,req,res e next.
+app.use((error,req,res,next)=>{
+  console.log("### Error Handler");
+  console.log(error);
+  res.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
