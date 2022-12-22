4abcc5e5d5a2237d2a9eec2371220f568e76a51e
AnaSousa <anasousa314@outlook.com>
2022-12-22 15:40:52 -0300

    MyContacts-aula38-Simple vs Preflighted requests

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,4 +1,10 @@
 module.exports = (request,response,next) => {
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    // response.setHeader('Access-Control-Allow-Headers', 'x-app-id');
+    // response.setHeader('Access-Control-Allow-Methods', 'GET, DELETE');
     next();
   }
+
+  // Header é case INSENSITIVE
