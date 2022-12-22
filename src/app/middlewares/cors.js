70b166851c7c8353b062415cc3a0c37716a7778e
AnaSousa <anasousa314@outlook.com>
2022-12-22 15:55:05 -0300

    MyContacts-aula39-cacheando resultado do Preflight

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -2,7 +2,9 @@ module.exports = (request,response,next) => {
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
     response.setHeader('Access-Control-Allow-Methods', '*');
     response.setHeader('Access-Control-Allow-Headers', '*');
-    // response.setHeader('Access-Control-Allow-Headers', 'x-app-id');
+    response.setHeader('Access-Control-Max-Age', '10');
+    // se não quisermos que tenha tempo de cache, basta colocar -1
+    // response.setHeader('Access-Control-Allow-Headers', 'x-app-id, content-type');
     // response.setHeader('Access-Control-Allow-Methods', 'GET, DELETE');
     next();
   }
